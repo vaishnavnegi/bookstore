086d2ebeb820740e7949ff51a7165b96aeec38d8
vaishnavnegi <vaishnavnegi207@gmail.com>
2020-11-10 21:38:10 +0530

    Updates

---
+++ precache-manifest.897e68aa8c80d2fcf26f8a91c377b68a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6bf228fd87e2a365cb4d6a5731952a76",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a9c261f537971facc8df",
+    "url": "/static/css/main.4cd98411.chunk.css"
+  },
+  {
+    "revision": "b636eaff3c564cc0af30",
+    "url": "/static/js/2.b96f2cdc.chunk.js"
+  },
+  {
+    "revision": "a9c261f537971facc8df",
+    "url": "/static/js/main.f2f3a85b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
